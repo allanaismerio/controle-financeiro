11cfd42a48505b94ae9b171855bdbfaa1388eb8f
Allana Ismerio <allanaismerio0@gmail.com>
2025-11-22 21:05:46 -0300

    Refactor database.py to use DeclarativeBase

--- database.py
+++ database.py
@@ -1,17 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-# Caminho do banco de dados SQLite
 DATABASE_URL = "sqlite:///./financeiro.db"
 
-# Criação do mecanismo de conexão
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
-# Criação da sessão do banco
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Classe base para os modelos
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
